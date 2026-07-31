57eb5ebc843b850878ec880edeaa3dc9c9c9c999
Omkar Ramayane <ramayaneomkar@gmail.com>
2026-07-31 23:26:19 +0530

    Time: 115 ms (7.27%), Space: 9.4 MB (75.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -3,19 +3,20 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int *ans=malloc(2*sizeof(int));
-
     for(int i=0;i<numsSize;i++)
     {
         for(int j=i+1;j<numsSize;j++)
         {
-            if(nums[i]+nums[j]==target)
-            {
-                ans[0]=i;
-                ans[1]=j;
-            }
+        int a=nums[i]+nums[j];
+        if(a==target)
+        {
+            ans[0]=i;
+            ans[1]=j;
+        }
         }
     }
     *returnSize=2;
     return ans;
+   
     
 }
