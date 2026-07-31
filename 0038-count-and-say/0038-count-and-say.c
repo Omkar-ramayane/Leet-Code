289ba59cf1efc852f0abdc90c3ab783a42ef88a5
Omkar Ramayane <ramayaneomkar@gmail.com>
2026-07-31 11:21:09 +0530

    Time: 9 ms (17.09%), Space: 42.5 MB (5.38%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.c
+++ 0038-count-and-say/0038-count-and-say.c
@@ -1,28 +1,29 @@
 char* countAndSay(int n) {
-    char*ans=(char*)malloc(50000);
-    strcpy(ans,"1");
-    for(int i=2;i<=n;i++)
-    {
-        char*temp=(char*)malloc(50000);
-        int k=0;
-        int c=1;
-        for(int j=0;ans[j]!='\0';j++)
+   char *ans=malloc(50000);
+   strcpy(ans,"1");
+   for(int i=2;i<=n;i++)
+   {
+     char *temp=malloc(500000);
+     int k=0;
+     int c=1;
+     for(int j=0;ans[j]!='\0';j++)
+     {
+        if(ans[j]==ans[j+1])
         {
-            if(ans[j]==ans[j+1])
-            {
-                c++;
-            }
-            else
-            {
-                temp[k++]=c+'0';
-                temp[k++]=ans[j];
-                c=1;
-            }
+            c++;
         }
-        temp[k]='\0';
-        free(ans);
-        ans=temp;
-    }
-    return ans;
+        else
+        {
+            temp[k++]=c+'0';
+            temp[k++]=ans[j];
+            c=1;
+        }
+     }
+     temp[k]='\0';
+     free(ans);
+     ans=temp;
+   }
+   
+   return ans;
     
 }
