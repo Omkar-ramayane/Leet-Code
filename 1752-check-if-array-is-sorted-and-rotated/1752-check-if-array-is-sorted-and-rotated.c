57123bfbbc5b5b6062f90af4d870f08e8646b759
Omkar Ramayane <ramayaneomkar@gmail.com>
2026-08-29 11:25:31 +0530

    Time: 0 ms (100%), Space: 8.7 MB (95.39%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.c
@@ -0,0 +1,21 @@
+bool check(int* nums, int numsSize) {
+    int c=0;
+    for(int i=0;i<numsSize-1;i++)
+    {
+        if(nums[i]>nums[i+1])
+        {
+            c++;
+        }
+    }
+    if(nums[0]<nums[numsSize-1])
+    {
+        c++;
+    }
+    if(c<=1)
+    {
+        return true;
+    }
+    return false;
+}
+   
+    
